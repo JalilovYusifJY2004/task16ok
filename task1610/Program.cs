b6acb2a5a34c1d5b5d66c7d28c12f82e2c0860b4
Yusif <145650476+JalilovYusifJY2004@users.noreply.github.com>
2023-10-15 23:02:54 +0400

    Update Program.cs

--- task1610/Program.cs
+++ task1610/Program.cs
@@ -38,6 +38,35 @@ namespace task1610
 
             //task3
 
+             Group group1 = new Group("Group 1");
+ Group group2 = new Group("Group 2");
+
+ Student student1 = new Student("Yusif", "Celilli");
+ Student student2 = new Student("Xeyal", "Huseynov");
+ Student student3 = new Student("Kenan", "Pasayev");
+ Student student4 = new Student("Samir", "Kerimov");
+
+ group1.AddStudent(student1);
+ group1.AddStudent(student2);
+ group2.AddStudent(student3);
+ group2.AddStudent(student4);
+
+ Group.ShowAllGroups();
+
+ Console.WriteLine();
+ group1.ShowStudents();
+ Console.WriteLine();
+ group2.ShowStudents();
+
+ Console.WriteLine();
+ group1.Search("Yusif");
+ group2.RemoveStudent(3);
+
+ Console.WriteLine();
+ group1.ShowStudents();
+ Console.WriteLine();
+ group2.ShowStudents();
+
 
 
 
@@ -128,6 +157,99 @@ namespace task1610
 
 
     //task3
+    class Student
+{
+    public static int Count { get; private set; } = 0;
+    public int Id { get; }
+    public string Name { get; }
+    public string Surname { get; }
+
+    public Student(string name, string surname)
+    {
+        Id = ++Count;
+        Name = name;
+        Surname = surname;
+    }
+
+    public string GetInfo()
+    {
+        return $"Student ID: {Id}, Name: {Name}, Surname: {Surname}";
+    }
+}
+
+class Group
+{
+    public static List<Group> Groups { get; } = new List<Group>();
+    public static int GroupCount => Groups.Count;
+    public int GroupId { get; }
+    public string GroupName { get; }
+    public List<Student> Students { get; } = new List<Student>();
+
+    public Group(string groupName)
+    {
+        GroupId = Groups.Count + 1;
+        GroupName = groupName;
+        Groups.Add(this);
+    }
+
+    public string GetGroupInfo()
+    {
+        return $"Group ID: {GroupId}, Group Name: {GroupName}, Number of Members: {Students.Count}";
+    }
+
+    public Student GetStudent(int id)
+    {
+        return Students.Find(student => student.Id == id);
+    }
+
+    public void AddStudent(Student student)
+    {
+        Students.Add(student);
+    }
+
+    public void Search(string searchString)
+    {
+        Console.WriteLine($"Searching for students with names containing '{searchString}':");
+        foreach (var student in Students)
+        {
+            if (student.Name.Contains(searchString, StringComparison.OrdinalIgnoreCase) || student.Surname.Contains(searchString, StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine(student.GetInfo());
+            }
+        }
+    }
+
+    public void RemoveStudent(int id)
+    {
+        Student studentToRemove = Students.Find(student => student.Id == id);
+        if (studentToRemove != null)
+        {
+            Students.Remove(studentToRemove);
+        }
+    }
+
+    public void ShowStudents()
+    {
+        Console.WriteLine($"Students in Group {GroupName}:");
+        foreach (var student in Students)
+        {
+            Console.WriteLine(student.GetInfo());
+        }
+    }
+
+    public static void ShowAllGroups()
+    {
+        Console.WriteLine("All Groups:");
+        foreach (var group in Groups)
+        {
+            Console.WriteLine(group.GetGroupInfo());
+        }
+    }
+
+    public static void AddGroup(Group group)
+    {
+        Groups.Add(group);
+    }
 
 
 
